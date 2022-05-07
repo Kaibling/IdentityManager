43fae73e87547ce5f7e966e23f4b72499076230e
Kaibling <l.rath@gmx.at>
2022-05-07 08:49:39 +0000

    add file

--- main.go
+++ main.go
@@ -6,14 +6,44 @@ import (
 
 	"github.com/Kaibling/IdentityManager/lib/cmd"
 	"github.com/Kaibling/IdentityManager/lib/config"
+	"github.com/Kaibling/IdentityManager/lib/database"
 	"github.com/Kaibling/IdentityManager/repositories/csv"
+	"github.com/Kaibling/IdentityManager/repositories/sqlite"
 	"github.com/Kaibling/IdentityManager/services"
 )
 
 func main() {
-	config.InitConfig()
-	csvRepo := csv.NewCSVRepo(config.Configuration.DBFilePath)
-	services.InitIdentityService(csvRepo)
+	err := config.InitConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	var r services.IdentityRepo
+	switch config.Configuration.Dialect {
+	case "SQLITE":
+		db, err := database.InitDBConnection()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		err = database.Migrate(db, []database.DBMigrator{sqlite.NewSQLiteMigrator(db)})
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		r, err = sqlite.NewSQLiteRepo(db)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	case "CSV":
+		r = csv.NewCSVRepo(config.Configuration.DBFilePath)
+	default:
+		fmt.Printf("DB Dialect '%s' not valid\n", config.Configuration.Dialect)
+		return
+	}
+
+	services.InitIdentityService(r)
 	args := os.Args[1:]
 	args = cmd.ParseFlags(args)
 	if len(args) == 0 {
@@ -29,7 +59,7 @@ func main() {
 	c.AddCommand("renew", services.Renew)
 	c.AddCommand("change", services.Change)
 
-	err := c.Exec(args)
+	err = c.Exec(args)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
